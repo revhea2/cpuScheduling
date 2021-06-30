d901a02021a74588851644464a0b995b1f9b2c2c
Brian Valencia <qbmsvalencia@tip.edu.ph>
2021-06-30 12:07:49 +0800

    Finished MLFB

--- schedulingAlgorithms/MultilevelFB.py
+++ schedulingAlgorithms/MultilevelFB.py
@@ -1,4 +1,4 @@
-from model.Table import Table
+from model.PreemptiveTable import PreemptiveTable
 from model.Process import Process
 
 class MultilevelFB:
@@ -28,8 +28,8 @@ class MultilevelFB:
 		second_level_finishers = []
 		third_level_finishers = []
 		# Quantum levels for the first 2 queues
-		first_quantum = 5
-		second_quantum = 10
+		first_quantum = 4
+		second_quantum = 8
 		# Tracker for the total time taken
 		time = 0
 		self.processes += processes
@@ -43,47 +43,76 @@ class MultilevelFB:
 		print(first_level)
 		while first_level:
 			process = first_level.pop(0)
+			if process.arrival_time > time:
+				time = process.arrival_time
 			new_burst = process.burst_time - first_quantum
+			# Either the process lasted first_quantum time units
+			# or process.burst_time units, depending on which is smaller
+			# if process.burst_time units gets picked, then the process
+			# is shorter than the quantum slice
+			burst_time=min(process.burst_time, first_quantum)
+			print(new_burst)
+			waiting_time = time - process.arrival_time
+			added_process = Process(process.name, process.arrival_time, burst_time=burst_time, interval=burst_time, start_time=time, waiting_time=waiting_time, turn_around_time=waiting_time+burst_time)
+
+			# A new_burst that's <= 0 implies a completed process
 			if new_burst <= 0:
-				new_burst = 0
-				# Either the process lasted first_quantum time units
-				# or process.burst_time units, depending on which is smaller
-				# if process.burst_time units gets picked, then the process
-				# is shorter than the quantum slice
-				added_process = Process(process.name, process.arrival_time, burst_time=min(process.burst_time, first_quantum), interval=min(process.burst_time, first_quantum), start_time=time, waiting_time=time-process.arrival_time, turn_around_time=time-process.arrival_time+min(process.burst_time, first_quantum))
-				process_list.append(added_process)
-				self.gantt_chart.append(added_process)
-				time += min(process.burst_time, second_quantum)
-				first_level_finishers.append(added_process)
-			# If the process is not finished, demote it 
+				added_process.completion_time = burst_time + time
+			# A new_burst that's > 0 implies demotion due to unfinished task
 			else:
 				process.burst_time -= first_quantum
 				second_level.append(process)
 
+			process.turn_around_time = added_process.turn_around_time
+			process_list.append(added_process)
+			self.gantt_chart.append(added_process)
+			time += burst_time
+			first_level_finishers.append(added_process)
+
+		print(second_level)
 		while second_level:
 			process = second_level.pop(0)
+			print(process.turn_around_time)
+			if process.arrival_time > time:
+				time = process.arrival_time
 			new_burst = process.burst_time - second_quantum
+			# Either the process lasted first_quantum time units
+			# or process.burst_time units, depending on which is smaller
+			# if process.burst_time units gets picked, then the process
+			# is shorter than the quantum slice
+			burst_time=min(process.burst_time, second_quantum)
+			print(new_burst)
+			waiting_time = time - process.arrival_time - process.turn_around_time
+			added_process = Process(process.name, process.arrival_time, burst_time=burst_time, interval=burst_time, start_time=time, waiting_time=waiting_time, turn_around_time=waiting_time+burst_time, waiting_queue_time=waiting_time)
+
+			# A new_burst that's <= 0 implies a completed process
 			if new_burst <= 0:
-				new_burst = 0
-				added_process = Process(process.name, process.arrival_time, burst_time=min(process.burst_time, second_quantum), start_time=time, interval=min(process.burst_time, second_quantum), waiting_time=time-process.arrival_time, turn_around_time=time-process.arrival_time+min(process.burst_time, second_quantum))
-				process_list.append(added_process)
-				time += min(process.burst_time, second_quantum)
-				self.gantt_chart.append(added_process)
-				second_level_finishers.append(added_process)
+				added_process.completion_time = burst_time + time
+			# A new_burst that's > 0 implies demotion due to unfinished task
 			else:
 				process.burst_time -= second_quantum
 				third_level.append(process)
+
+			process_list.append(added_process)
+			self.gantt_chart.append(added_process)
+			time += burst_time
+			second_level_finishers.append(added_process)
 		
 		while third_level:
 			process = third_level.pop(0)
-			process.interval = process.burst_time
-			process.start_time = time
-			time += process.burst_time
 			self.gantt_chart.append(process)
-			process_list.append(added_process)
+			waiting_time = time - process.arrival_time - process.turn_around_time
+			process.waiting_time = waiting_time
+			process.start_time = time
+			process.waiting_queue_time = waiting_time
+			process.completion_time = process.start_time + process.burst_time
+			process.interval = process.burst_time
+			process.turn_around_time = process.waiting_time + process.burst_time
+			process_list.append(process)
+			time += process.burst_time
 			third_level_finishers.append(process)
-		
-		self.table = Table(process_list)
+
+		self.table = PreemptiveTable(process_list)
 		return (first_level_finishers,second_level_finishers,third_level_finishers, self.gantt_chart, self.table)
 
 
